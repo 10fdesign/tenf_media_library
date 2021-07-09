785e10c3d6deadd8bf3a36011eebad32b949269a
Connor Deachman <connor@10fdesign.io>
2021-07-09 12:15:02 -0400

    media objects have a preview file, used currently for PDFs (and in the future, videos?)

--- app/models/media_object.rb
+++ app/models/media_object.rb
@@ -4,11 +4,23 @@ class MediaObject < ApplicationRecord
 
 	belongs_to :media_directory, optional: true
   has_one_attached :file
+  has_one_attached :preview
   validates :file, :presence => true
 
 	HERO = [1920, 500].freeze
 	TILE = [540, 360].freeze
 	SMALLTILE = [255, 200].freeze
 	THUMBNAIL = [nil, 100].freeze
+
+  def image
+    if preview.attached?
+      return preview
+    elsif file.attached?
+      return file
+    else
+      puts "uh oh! calling #image on media_object with no attached file or preview :("
+      return nil
+    end
+  end
 
 end
