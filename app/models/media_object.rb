9d693041b9e775f5a0c9368308a29c76dbdbe9fb
Benny Heller <echo29x@gmail.com>
2021-07-29 13:01:28 -0400

    moved pdf preview creation to after_commmit callback

--- app/models/media_object.rb
+++ app/models/media_object.rb
@@ -2,6 +2,8 @@ require 'administrate/field/active_storage'
 
 class MediaObject < ApplicationRecord
 
+	after_create_commit :handle_pdf
+
 	belongs_to :media_directory, optional: true
   has_one_attached :file
   has_one_attached :preview
@@ -21,6 +23,33 @@ class MediaObject < ApplicationRecord
       puts "uh oh! calling #image on media_object with no attached file or preview :("
       return nil
     end
+  end
+
+  def handle_pdf
+  	if self.file.content_type.start_with? "application/pdf"
+	  	pdf = MiniMagick::Image.open self.file
+	    puts "DEBUG"
+	    file_name = self.file.attachment.filename
+	    puts "file_name #{file_name}"
+	    converted_file_path = File.join(Dir.tmpdir, "#{file_name}-#{Time.now.strftime("%Y%m%d")}-#{$$}-#{rand(0x100000000).to_s(36)}.png")
+	    puts "converted_file_path #{converted_file_path}"
+	    converted_file_path.gsub!(".pdf", "")
+	    puts "converted_file_path #{converted_file_path}"
+	    convert_result = MiniMagick::Tool::Convert.new do |convert|
+	      convert.background "white"
+	      convert.flatten
+	      convert.density 150
+	      convert.quality 100
+	      convert << pdf.pages.first.path
+	      convert << converted_file_path
+	    end
+	    puts "convert_result #{convert_result}"
+	    
+	    attach_result = self.preview.attach(io: File.open(converted_file_path), filename: file_name.to_s.gsub(".pdf", ".png"), content_type: "image/png")
+	    puts "attach_result #{attach_result}"
+	    rm_result = FileUtils.rm(converted_file_path)
+	    puts "rm_result #{rm_result}"
+	  end
   end
 
 end
