d12f7512c8335b5095c2edaad7e1233ff737c6fb
Benny Heller <echo29x@gmail.com>
2022-07-20 12:02:13 -0400

    fixed bootstrap path for production asset compiling

--- app/assets/javascripts/tenf_media_library/tenf_media_library.js
+++ app/assets/javascripts/tenf_media_library/tenf_media_library.js
@@ -4,5 +4,5 @@
 
 //= require jquery-ui-dist
 //  require jquery-ui/ui/widgets/sortable
-//= require bootstrap
+//  require bootstrap
 //= require administrate/many_media_objects_field
--- lib/tenf_media_library.rb
+++ lib/tenf_media_library.rb
@@ -6,15 +6,15 @@ require 'administrate/field/active_storage'
 module TenfMediaLibrary
   class Engine < Rails::Engine
     initializer 'administrate.assets.precompile' do |app|
-      app.config.assets.precompile << 'bootstrap.js' if app.config.respond_to? :assets
-      Administrate::Engine.add_javascript 'bootstrap.js' if defined?(Administrate::Engine)
+      app.config.assets.precompile << 'bootstrap/dist/js/bootstrap.js' if app.config.respond_to? :assets
+      Administrate::Engine.add_javascript 'bootstrap/dist/js/bootstrap.js' if defined?(Administrate::Engine)
 
       app.config.assets.precompile << 'jquery-ui-dist/jquery-ui.js' if app.config.respond_to? :assets
       Administrate::Engine.add_javascript 'jquery-ui-dist/jquery-ui.js' if defined?(Administrate::Engine)
 
 	    app.config.assets.precompile << 'administrate/media_objects.js' if app.config.respond_to? :assets
-	      Administrate::Engine.add_javascript 'administrate/media_objects.js' if defined?(Administrate::Engine)
-	    end
+      Administrate::Engine.add_javascript 'administrate/media_objects.js' if defined?(Administrate::Engine)
+    end
 
     initializer "administrate.assets.precompile" do |app|
       app.config.assets.precompile += %w( media_library.css )
